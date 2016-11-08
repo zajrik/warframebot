ac45f25020677baf3dfeb570fbfb8baaf619d1f9
Zack Campbell <zajrik@gmail.com>
2016-11-07 20:43:07 -0600

    Rename expired notification prune timer

--- src/lib/WfBot.ts
+++ src/lib/WfBot.ts
@@ -24,6 +24,6 @@ export default class WfBot extends Bot
 		this.timers.add(new Timer(this, 'events', 2 * 60, async () => this.eventLoader.loadEvents()));
 		this.timers.add(new Timer(this, 'items', 5 * 60 * 60, async () => this.itemLoader.reloadItems()));
 		this.timers.add(new Timer(this, 'notify', 60, async () => this.notifier.checkEvents()));
-		this.timers.add(new Timer(this, 'pruneNotifications', 5 * 60, async () => this.notifier.checkExpired()));
+		this.timers.add(new Timer(this, 'prune', 5 * 60, async () => this.notifier.checkExpired()));
 	}
 }
